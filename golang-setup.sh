aa149aac178c371805ebea1451168c5e0c1f8bd8
Azzen Abidi <azzen.abidi@gmail.com>
2022-01-01 22:10:08 +0000

    deleted golang tarballs

--- golang-setup.sh
+++ golang-setup.sh
@@ -1,6 +1,6 @@
 sudo apt update 
 wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin">>/etc/profile
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+sudo echo "export PATH=$PATH:/usr/local/go/bin">>/etc/profile
 go version
 
