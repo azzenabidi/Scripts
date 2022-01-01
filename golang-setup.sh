441e87203edde873cf3b8943854c08ea63bd04c5
Azzen Abidi <azzen.abidi@gmail.com>
2022-01-01 15:36:52 +0000

    added golang setup script

---
+++ golang-setup.sh
@@ -0,0 +1,6 @@
+sudo apt update 
+wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin">>/etc/profile
+go version
+
